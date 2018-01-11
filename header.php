b2414397b21c8e64da01f2e1e9a193a3b1dca8c0
oyeu <34932594+oyeu@users.noreply.github.com>
2018-01-11 14:27:59 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Centro Psique</title>
+  </head>
+  <body>
+    <main>
+
+        <div class="container">
+          <div class="row center-xs">
+            <div class="col-md-3">
+                <img class="img-responsive img-rounded center-block logo" src="./Images\LOGO PSIQUE.PNG" alt="logoPsique">
+            </div>
+            <div class="col-md-7">
+              <div class="row">
+                <div class="container container-buscador">
+                  <form class="buscador">
+                    <div class="input-group">
+                      <input class="form-control-xs" type="text" placeholder="Buscar..." aria-label="Search" style="border-radius:20px;">
+                      <button class="btn btn-link btn-xs" type="submit" >
+                        <img src="./Images/lupa.jpg" alt="asd" class="img-responsive center-block lupa" width="21.3px">
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="container menu-principal">
+                  <nav class="navbar navbar-expand-sm navbar-light bg-link">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+                      <ul class="navbar-nav">
+                        <li class="nav-item">
+                          <a class="nav-link" href="#" ><img class="botones-menu" src="./Images/imagenes menu/Botón inicio.png"></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#"><img class="botones-menu" src="./Images/imagenes menu/Botón Servicios.png"></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#"><img class="botones-menu" src="./Images/imagenes menu/Botón Eventos.png"></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#"><img class="botones-menu" src="./Images/imagenes menu/Botón Quiénes somos.png"></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#"><img class="botones-menu" src="./Images/imagenes menu/Botón Contactanos.png"></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+
+              </div>
+
+
+
+            </div>
+            <div class="col-md-2">
+
+            </div>
+          </div>
+        </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
